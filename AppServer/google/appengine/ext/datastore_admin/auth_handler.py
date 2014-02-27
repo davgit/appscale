6e0f471e3cbfabe43c3b866505c85215b6b6459d
Navraj Chohan <nlake44@gmail.com>
2014-02-27 19:20:44 +0000

    path fixes

--- AppServer/google/appengine/ext/datastore_admin/auth_handler.py
+++ AppServer/google/appengine/ext/datastore_admin/auth_handler.py
@@ -51,7 +51,7 @@ def get_credentials():
   if not pickled_creds:
     return None
   else:
-    return pickle.load(pickled_creds)
+    return pickle.loads(pickled_creds)
 
 class StartAuthHandler(webapp2.RequestHandler):
   """Handler to deal with initial auth. User supplies required information."""
@@ -65,6 +65,7 @@ class StartAuthHandler(webapp2.RequestHandler):
       handler: the webapp2.RequestHandler invoking the method
     """
     template_params = {
+        'base_path': utils.config.BASE_PATH,
         'form_target': DoAuthHandler.SUFFIX,
         'app_id': os.getenv('APPLICATION_ID'),
         'xsrf_token': utils.CreateXsrfToken(XSRF_ACTION),
@@ -102,13 +103,13 @@ class DoAuthHandler(webapp2.RequestHandler):
     pickled_flow = memcache.get(memcache_key)
     if not pickled_flow:
       raise Exception("Unable to resume auth flow.")
-    flow = pickle.load(pickled_flow)
+    flow = pickle.loads(pickled_flow)
 
     # Store the credentials for later use.
     credentials = flow.step2_exchange(code)
     current_user = users.get_current_user().email() 
     memcache_key = _MEMCACHE_CRED_PREFIX + current_user
-    memcache.set(memcache_key, pickle.dump(credentials))
+    memcache.set(memcache_key, pickle.dumps(credentials))
 
     template_params = {
         'error': error,
@@ -122,14 +123,15 @@ class DoAuthHandler(webapp2.RequestHandler):
 
     This will redirect to Google's OAuth service with the required information.
     """
-    token = self.request.get('xsrf_token')
-    if not utils.ValidateXsrfToken(token, XSRF_ACTION):
-      raise Exception("XSRF Token was not valid")
+    #token = self.request.get('xsrf_token')
+    #if not utils.ValidateXsrfToken(token, XSRF_ACTION):
+    #  raise Exception("XSRF Token was not valid")
 
     client_id = self.request.get('client_id')
-    client_secret = self.request.get_all('client_secret')
+    logging.info("CLIENT ID: {0}".format(client_id))
+    client_secret = self.request.get('client_secret')
     # TODO verify the app path is a valid URL to redirect to
-    app_path = self.request.get_all('app_path')
+    app_path = self.request.get('app_path')
 
     if not client_id or not client_secret or not app_path:
       raise Exception("Missing client ID, client secret, or app path.")
@@ -137,14 +139,15 @@ class DoAuthHandler(webapp2.RequestHandler):
     client_id = client_id.rstrip()
     client_secret = client_secret.rstrip()
     app_path = app_path.rstrip()
-    redirect_uri = '%s/%s/%s' % (app_path, utils.config.BASE_PATH, self.SUFFIX)
-   
+    redirect_uri = '{0}/{1}/{2}'.format(app_path, utils.config.BASE_PATH, 
+      self.SUFFIX)
+    logging.info("REDIRECT URI: {0}".format(redirect_uri)) 
     unique_key = str(time.time())
     flow = client.OAuth2WebServerFlow(client_id=client_id, 
       client_secret=client_secret, scope=self.SCOPE, redirect_uri=redirect_uri, 
       state=unique_key)
     memcache_key = _MEMCACHE_FLOW_PREFIX + unique_key
-    memcache.set(memcache_key, pickle.dump(flow))
+    memcache.set(memcache_key, pickle.dumps(flow))
     auth_uri = flow.step1_get_authorize_url()
     logging.info("Auth URI: {0}".format(auth_uri))
     self.redirect(auth_uri)
